457b06b1534286c6712fe63f95ec64f924fb896e
fg-61 <fatihgoren61@gmail.com>
2021-11-16 14:50:14 +0300

    Classların içi dolduruldu

---
+++ CafeDemoLib/Models/Urun.cs
@@ -0,0 +1,10 @@
+﻿using CafeDemoLib.Sınıflar;
+
+namespace CafeDemoLib.Data
+{
+    public class Urun : Kategori
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+    }
+}
--- CafeDemoLib/Sınıflar/Urunler.cs
+++ CafeDemoLib/Sınıflar/Urunler.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CafeDemoLib.Data;
 
 namespace CafeDemoLib.Sınıflar
 {
     public class Urunler
     {
-        public string Adi { get; set; }
-        public decimal Fiyat { get; set; }
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
         public string Kategori { get; set; }
         public byte[] Resim { get; set; }
         public bool Durum { get; set; }
-
     }
 }
