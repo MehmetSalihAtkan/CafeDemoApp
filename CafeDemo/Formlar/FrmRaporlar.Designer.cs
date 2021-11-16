bd84ae449e1a236f80390915d09eb59cca30903a
sefaa-dev <sefayilmaz582@gmail.com>
2021-11-16 15:00:23 +0300

    Rapor tamam gibi

--- CafeDemo/Formlar/FrmRaporlar.Designer.cs
+++ CafeDemo/Formlar/FrmRaporlar.Designer.cs
@@ -29,11 +29,50 @@ namespace CafeDemo.Formlar
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Hamburger",
+            "200",
+            "18 ",
+            "₺"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Lahmacun",
+            "150",
+            "26 ",
+            "₺"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Künefe",
+            "60",
+            "12 ",
+            "₺"}, -1);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmRaporlar));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtToplam = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblKonum = new System.Windows.Forms.Label();
+            this.btnCikis = new System.Windows.Forms.Button();
+            this.btnYazdır = new System.Windows.Forms.Button();
+            this.btnBaskiOnizleme = new System.Windows.Forms.Button();
+            this.btnSayfaAyarlari = new System.Windows.Forms.Button();
+            this.btnSonKayit = new System.Windows.Forms.Button();
+            this.btnileri = new System.Windows.Forms.Button();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.btnilkKayit = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.pageSetupDialog1 = new System.Windows.Forms.PageSetupDialog();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDialog1 = new System.Windows.Forms.PrintDialog();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
             this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -48,25 +87,197 @@ namespace CafeDemo.Formlar
             this.tabControl1.Multiline = true;
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(800, 450);
+            this.tabControl1.Size = new System.Drawing.Size(906, 451);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.listView1);
             this.tabPage1.Location = new System.Drawing.Point(4, 46);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(792, 400);
+            this.tabPage1.Size = new System.Drawing.Size(898, 401);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Günlük Rapor";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtToplam);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblKonum);
+            this.groupBox1.Controls.Add(this.btnCikis);
+            this.groupBox1.Controls.Add(this.btnYazdır);
+            this.groupBox1.Controls.Add(this.btnBaskiOnizleme);
+            this.groupBox1.Controls.Add(this.btnSayfaAyarlari);
+            this.groupBox1.Controls.Add(this.btnSonKayit);
+            this.groupBox1.Controls.Add(this.btnileri);
+            this.groupBox1.Controls.Add(this.btnGeri);
+            this.groupBox1.Controls.Add(this.btnilkKayit);
+            this.groupBox1.Location = new System.Drawing.Point(545, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(346, 219);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtToplam
+            // 
+            this.txtToplam.Location = new System.Drawing.Point(178, 65);
+            this.txtToplam.Name = "txtToplam";
+            this.txtToplam.Size = new System.Drawing.Size(157, 43);
+            this.txtToplam.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 37);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Toplam Tutar";
+            // 
+            // lblKonum
+            // 
+            this.lblKonum.AutoSize = true;
+            this.lblKonum.Location = new System.Drawing.Point(138, 11);
+            this.lblKonum.Name = "lblKonum";
+            this.lblKonum.Size = new System.Drawing.Size(88, 37);
+            this.lblKonum.TabIndex = 9;
+            this.lblKonum.Text = "10/10";
+            // 
+            // btnCikis
+            // 
+            this.btnCikis.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnCikis.Location = new System.Drawing.Point(162, 163);
+            this.btnCikis.Name = "btnCikis";
+            this.btnCikis.Size = new System.Drawing.Size(111, 32);
+            this.btnCikis.TabIndex = 8;
+            this.btnCikis.Text = "Çıkış";
+            this.btnCikis.UseVisualStyleBackColor = true;
+            this.btnCikis.Click += new System.EventHandler(this.btnCikis_Click);
+            // 
+            // btnYazdır
+            // 
+            this.btnYazdır.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnYazdır.Location = new System.Drawing.Point(9, 167);
+            this.btnYazdır.Name = "btnYazdır";
+            this.btnYazdır.Size = new System.Drawing.Size(95, 28);
+            this.btnYazdır.TabIndex = 7;
+            this.btnYazdır.Text = "Yazdır";
+            this.btnYazdır.UseVisualStyleBackColor = true;
+            this.btnYazdır.Click += new System.EventHandler(this.btnYazdır_Click);
+            // 
+            // btnBaskiOnizleme
+            // 
+            this.btnBaskiOnizleme.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBaskiOnizleme.Location = new System.Drawing.Point(162, 119);
+            this.btnBaskiOnizleme.Name = "btnBaskiOnizleme";
+            this.btnBaskiOnizleme.Size = new System.Drawing.Size(111, 33);
+            this.btnBaskiOnizleme.TabIndex = 6;
+            this.btnBaskiOnizleme.Text = "Baskı Önizleme";
+            this.btnBaskiOnizleme.UseVisualStyleBackColor = true;
+            this.btnBaskiOnizleme.Click += new System.EventHandler(this.btnBaskiOnizleme_Click);
+            // 
+            // btnSayfaAyarlari
+            // 
+            this.btnSayfaAyarlari.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSayfaAyarlari.Location = new System.Drawing.Point(9, 119);
+            this.btnSayfaAyarlari.Name = "btnSayfaAyarlari";
+            this.btnSayfaAyarlari.Size = new System.Drawing.Size(95, 33);
+            this.btnSayfaAyarlari.TabIndex = 5;
+            this.btnSayfaAyarlari.Text = "Sayfa Ayarları";
+            this.btnSayfaAyarlari.UseVisualStyleBackColor = true;
+            this.btnSayfaAyarlari.Click += new System.EventHandler(this.btnSayfaAyarlari_Click);
+            // 
+            // btnSonKayit
+            // 
+            this.btnSonKayit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSonKayit.Location = new System.Drawing.Point(279, 12);
+            this.btnSonKayit.Name = "btnSonKayit";
+            this.btnSonKayit.Size = new System.Drawing.Size(41, 36);
+            this.btnSonKayit.TabIndex = 4;
+            this.btnSonKayit.Text = ">>";
+            this.btnSonKayit.UseVisualStyleBackColor = true;
+            this.btnSonKayit.Click += new System.EventHandler(this.btnSonKayit_Click);
+            // 
+            // btnileri
+            // 
+            this.btnileri.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnileri.Location = new System.Drawing.Point(232, 12);
+            this.btnileri.Name = "btnileri";
+            this.btnileri.Size = new System.Drawing.Size(41, 36);
+            this.btnileri.TabIndex = 3;
+            this.btnileri.Text = ">";
+            this.btnileri.UseVisualStyleBackColor = true;
+            this.btnileri.Click += new System.EventHandler(this.btnileri_Click);
+            // 
+            // btnGeri
+            // 
+            this.btnGeri.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnGeri.Location = new System.Drawing.Point(91, 12);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(41, 36);
+            this.btnGeri.TabIndex = 2;
+            this.btnGeri.Text = "<";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // btnilkKayit
+            // 
+            this.btnilkKayit.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnilkKayit.Location = new System.Drawing.Point(40, 11);
+            this.btnilkKayit.Name = "btnilkKayit";
+            this.btnilkKayit.Size = new System.Drawing.Size(41, 36);
+            this.btnilkKayit.TabIndex = 1;
+            this.btnilkKayit.Text = "<<";
+            this.btnilkKayit.UseVisualStyleBackColor = true;
+            this.btnilkKayit.Click += new System.EventHandler(this.btnilkKayit_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2,
+            listViewItem3});
+            this.listView1.Location = new System.Drawing.Point(6, 6);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(532, 359);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ürün Adı";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Ürün Adet";
+            this.columnHeader2.Width = 140;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Fiyat";
+            this.columnHeader3.Width = 100;
             // 
             // tabPage2
             // 
             this.tabPage2.Location = new System.Drawing.Point(4, 46);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(792, 244);
+            this.tabPage2.Size = new System.Drawing.Size(898, 401);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Haftalık Rapor";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -76,16 +287,44 @@ namespace CafeDemo.Formlar
             this.tabPage3.Location = new System.Drawing.Point(4, 46);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(792, 244);
+            this.tabPage3.Size = new System.Drawing.Size(898, 401);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Aylık Rapor";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // pageSetupDialog1
+            // 
+            this.pageSetupDialog1.Document = this.printDocument1;
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Document = this.printDocument1;
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDialog1
+            // 
+            this.printDialog1.UseEXDialog = true;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "";
+            this.columnHeader4.Width = 30;
             // 
             // FrmRaporlar
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(906, 451);
             this.ControlBox = false;
             this.Controls.Add(this.tabControl1);
             this.MaximizeBox = false;
@@ -95,6 +334,9 @@ namespace CafeDemo.Formlar
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.FrmRaporlar_Load);
             this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -105,5 +347,26 @@ namespace CafeDemo.Formlar
         private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtToplam;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblKonum;
+        private System.Windows.Forms.Button btnCikis;
+        private System.Windows.Forms.Button btnYazdır;
+        private System.Windows.Forms.Button btnBaskiOnizleme;
+        private System.Windows.Forms.Button btnSayfaAyarlari;
+        private System.Windows.Forms.Button btnSonKayit;
+        private System.Windows.Forms.Button btnileri;
+        private System.Windows.Forms.Button btnGeri;
+        private System.Windows.Forms.Button btnilkKayit;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PageSetupDialog pageSetupDialog1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Windows.Forms.PrintDialog printDialog1;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
--- CafeDemo/Formlar/FrmRaporlar.cs
+++ CafeDemo/Formlar/FrmRaporlar.cs
@@ -19,6 +19,141 @@ namespace CafeDemo.Formlar
 
         private void FrmRaporlar_Load(object sender, EventArgs e)
         {
+            listView1.Items[0].Selected = true;
+            SayfaBilgisiAl();
+            ToplamAl();
+        }
+        private void ToplamAl()
+        {
+            decimal toplam = 0;
+            for (int i = 0; i < listView1.Items.Count; i++)
+            {
+                toplam += Convert.ToDecimal(listView1.Items[i].SubItems[2].Text);
+            }
+            txtToplam.Text = toplam.ToString("#,###.00 TL");
+        }
+
+        private void btnBaskiOnizleme_Click(object sender, EventArgs e)
+        {
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void btnilkKayit_Click(object sender, EventArgs e)
+        {
+            listView1.Items[0].Selected = true;
+            listView1.Focus();
+            SayfaBilgisiAl();
+        }
+        private void SayfaBilgisiAl()
+        {
+            lblKonum.Text = (listView1.SelectedIndices[0] + 1).ToString() + "/" + listView1.Items.Count.ToString();
+        }
+
+        private void btnGeri_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices[0] > 0)
+            {
+                listView1.Items[listView1.SelectedIndices[0] - 1].Selected = true;
+            }
+            listView1.Focus();
+            SayfaBilgisiAl();
+        }
+
+        private void btnileri_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices[0] < listView1.Items.Count - 1)
+            {
+                listView1.Items[listView1.SelectedIndices[0] + 1].Selected = true;
+            }
+            listView1.Focus();
+            SayfaBilgisiAl();
+        }
+
+        private void btnSonKayit_Click(object sender, EventArgs e)
+        {
+            listView1.Items[listView1.Items.Count - 1].Selected = true;
+            listView1.Focus();
+            SayfaBilgisiAl();
+        }
+        private void listView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            SayfaBilgisiAl();
+        }
+
+        private void listView1_KeyUp(object sender, KeyEventArgs e)
+        {
+            SayfaBilgisiAl();
+        }
+
+        private void btnCikis_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSayfaAyarlari_Click(object sender, EventArgs e)
+        {
+            pageSetupDialog1.ShowDialog();
+        }
+
+        private void btnYazdır_Click(object sender, EventArgs e)
+        {
+            DialogResult pdr = printDialog1.ShowDialog();
+            if (pdr == DialogResult.OK)
+            {
+                printDocument1.Print();
+            }
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            //Yazı fontumu ve çizgi çizmek için fırçamı ve kalem nesnemi oluşturdum
+            Font myFont = new Font("Calibri", 28);
+            SolidBrush sbrush = new SolidBrush(Color.Black);
+            Pen myPen = new Pen(Color.Black);
+
+            e.Graphics.DrawImage(Properties.Resources.logo, 100, 800);
+
+            //Bu kısımda sipariş formu yazısını ve çizgileri yazdırıyorum
+            e.Graphics.DrawLine(myPen, 120, 120, 750, 120);
+            e.Graphics.DrawLine(myPen, 120, 180, 750, 180);
+            e.Graphics.DrawString("WİSSEN BİSTRO SİPARİŞ FORMU", myFont, sbrush, 180, 120);
+
+            e.Graphics.DrawLine(myPen, 120, 320, 750, 320);
+
+            myFont = new Font("Calibri", 12, FontStyle.Bold);
+            e.Graphics.DrawString("Adet", myFont, sbrush, 140, 328);
+            e.Graphics.DrawString("Ürün Adı", myFont, sbrush, 240, 328);
+            e.Graphics.DrawString("Birim Fiyatı", myFont, sbrush, 440, 328);
+            e.Graphics.DrawString("Fiyat", myFont, sbrush, 640, 328);
+
+            e.Graphics.DrawLine(myPen, 120, 348, 750, 348);
+
+            int y = 360;
+
+            StringFormat myStringFormat = new StringFormat();
+            myStringFormat.Alignment = StringAlignment.Far;
+
+            decimal gTotal = 0;
+
+            foreach (ListViewItem lvi in listView1.Items)
+            {
+                e.Graphics.DrawString(lvi.SubItems[1].Text, myFont, sbrush, 160, y, myStringFormat);
+                e.Graphics.DrawString(lvi.Text, myFont, sbrush, 220, y);
+                decimal bFiyat = Convert.ToDecimal(lvi.SubItems[2].Text);
+                decimal fiyat = Convert.ToDecimal(lvi.SubItems[1].Text) * Convert.ToDecimal(lvi.SubItems[2].Text);
+                gTotal += fiyat;
+                e.Graphics.DrawString(bFiyat.ToString("c"), myFont, sbrush, 530, y, myStringFormat);
+                e.Graphics.DrawString(fiyat.ToString("c"), myFont, sbrush, 700, y, myStringFormat);
+
+                y += 20;
+
+            }
+
+            e.Graphics.DrawLine(myPen, 120, y, 750, y);
+            e.Graphics.DrawString(gTotal.ToString("c"), myFont, sbrush, 700, y + 10, myStringFormat);
+            
+            
+
 
         }
     }
